5ab7fdcc9832f38bd0b1368017e23aa69b325fbe
TanmayDangat <dangattanmay204@gmail.com>
2026-05-15 21:15:35 +0530

    Added new file

--- src/test/java/com/orangehrm/test/LoginPageTest.java
+++ src/test/java/com/orangehrm/test/LoginPageTest.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 import com.orangehrm.base.BaseClass;
 import com.orangehrm.pages.HomePage;
 import com.orangehrm.pages.LoginPage;
+import com.orangehrm.utilities.DataProviders;
 import com.orangehrm.utilities.ExtentManager;
 
 public class LoginPageTest extends BaseClass {
